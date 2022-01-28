d0da37dd203d8d82099cd9730b7b38a12172d6c9
Michael Ymbong <5406654+michaelymbong@users.noreply.github.com>
2022-01-28 21:56:04 +0800

    feat(all): retrieve crpyto info from coinmarketcap api

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const cron = require("node-cron");
+const dotenv = require("dotenv");
+const axios = require("axios");
 
-cron.schedule("* * * * *", () => {
-  console.log("running...");
+dotenv.config();
+
+cron.schedule("* 8 * * *", () => {
+  console.log("getting coin info...");
+  getCoinInfo();
 });
+
+const getCoinInfo = async () => {
+  try {
+    const options = {
+      url: `${process.env.COIN_API_BASE_URL}/v1/cryptocurrency/listings/latest`,
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.COIN_API_KEY,
+      },
+      params: {
+        limit: 15,
+      },
+      validateStatus: (status) => status <= 200,
+    };
+
+    const response = await axios(options);
+    console.log(response.data);
+
+    console.log("coin info retrieved");
+  } catch (error) {
+    console.error(error);
+  }
+};
