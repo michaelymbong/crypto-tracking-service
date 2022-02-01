1f8e65edba30b10aee7aa66cbd890037d5430c36
Michael Ymbong <5406654+michaelymbong@users.noreply.github.com>
2022-02-01 15:22:18 +0800

    refactor(package): use type module for node import settings

--- index.js
+++ index.js
@@ -1,68 +1,13 @@
-const cron = require("node-cron");
-const dotenv = require("dotenv");
-const axios = require("axios");
+import cron from "node-cron";
+import dotenv from "dotenv";
+import { getCoinInfo, processCoinInfo } from "./src/coin.js";
 
+// Load environment variables from env file
 dotenv.config();
 
 cron.schedule("* 8 * * *", () => {
   root();
 });
-
-const getCoinInfo = async () => {
-  let coins = [];
-
-  try {
-    const options = {
-      url: `${process.env.COIN_API_BASE_URL}/v1/cryptocurrency/listings/latest`,
-      headers: {
-        "X-CMC_PRO_API_KEY": process.env.COIN_API_KEY,
-      },
-      params: {
-        limit: 15,
-      },
-      validateStatus: (status) => status <= 200,
-    };
-
-    const response = await axios(options);
-    coins = response.data.data;
-
-    console.log("coin info retrieved");
-  } catch (error) {
-    console.error(error);
-  }
-
-  return coins;
-};
-
-const processCoinInfo = (coin) => {
-  const {
-    id,
-    name,
-    max_supply,
-    circulating_supply,
-    total_supply,
-    quote: {
-      USD: {
-        price,
-        market_cap,
-        market_cap_dominance,
-        fully_diluted_market_cap,
-      },
-    },
-  } = coin;
-
-  return {
-    id,
-    name,
-    max_supply,
-    circulating_supply,
-    total_supply,
-    price,
-    market_cap,
-    market_cap_dominance,
-    fully_diluted_market_cap,
-  };
-};
 
 const root = async () => {
   console.log("getting coin info...");
---
+++ src/coin.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+export const getCoinInfo = async () => {
+  let coins = [];
+
+  try {
+    const options = {
+      url: `${process.env.COIN_API_BASE_URL}/v1/cryptocurrency/listings/latest`,
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.COIN_API_KEY,
+      },
+      params: {
+        limit: 15,
+      },
+      validateStatus: (status) => status <= 200,
+    };
+
+    const response = await axios(options);
+    coins = response.data.data;
+
+    console.log("coin info retrieved");
+  } catch (error) {
+    console.error(error);
+  }
+
+  return coins;
+};
+
+export const processCoinInfo = (coin) => {
+  const {
+    id,
+    name,
+    max_supply,
+    circulating_supply,
+    total_supply,
+    quote: {
+      USD: {
+        price,
+        market_cap,
+        market_cap_dominance,
+        fully_diluted_market_cap,
+      },
+    },
+  } = coin;
+
+  return {
+    id,
+    name,
+    max_supply,
+    circulating_supply,
+    total_supply,
+    price,
+    market_cap,
+    market_cap_dominance,
+    fully_diluted_market_cap,
+  };
+};
